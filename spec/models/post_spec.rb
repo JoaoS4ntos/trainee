1b1fe5199b6098c40f13496dd2cc6792ea5a469e
Laguni <jpfsantos2004@gmail.com>
2023-06-18 23:42:18 -0300

    Teste title not null passed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  context "Testing factory" do
-  it {expect(build(:post)).to be_valid}
+  context "Check title and content not null"
+    it "title ok" do
+      novopost=Post.new(title:"news",content:"conteudo")
+      expect(novopost).to be_valid
+    end
+    it "title not ok" do
+      novopost=Post.new(title:nil,content:nil)
+      expect(novopost).to_not be_valid
+    end
   end
-end
+
