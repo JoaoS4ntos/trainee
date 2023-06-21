0a92157a62260ccd4a8163384814b6e0873c9fa8
Laguni <jpfsantos2004@gmail.com>
2023-06-20 22:35:36 -0300

    post/create test passed

--- app/controllers/api/post_controller.rb
+++ app/controllers/api/post_controller.rb
@@ -1,3 +1,15 @@
 class Api::PostController < ApplicationController
-    
+    def create
+        post=Post.new(post_params)
+        post.save!
+        render json: post, status: :created
+    rescue StandardError => e
+        render json: e, status: :bad_request
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:id , :title, :content)
+    end
 end
--- spec/requests/api/post_spec.rb
+++ spec/requests/api/post_spec.rb
@@ -11,5 +11,13 @@ RSpec.describe "Api::Posts", type: :request do
         expect(response).to have_http_status(:created)
       end
     end
+    
+    context "when params are nil"
+      post_params=nil
+      it "return http status bad_request" do
+        post "/api/post/create" , params:{post: post_params}
+        expect(response).to have_http_status(:bad_request)
+      end
+    end
   end
-end
+
